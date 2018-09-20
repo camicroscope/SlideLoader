f31eab7925cf17aa006e9e8915631afe69194242
Ryan Birmingham <birm@rbirm.us>
2018-09-20 16:07:47 -0400

    tested for slide metadata single and many

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # SlideLoader
-Tool for loading slides and getting metadata
+Tool for loading slides and getting slide metadata using openslide
 
 ## Usage
-TODO
+
+### Upload
+
+### Get a Single Image's Metadata
+
+See, for example, http://<service url base>/data/one/<slide filename>
+
+This returns the metadata for the given slide.
+### Get metadata for a list of image
+
+See, for example http://<service url base>/data/many/["<slide1_filename", "slide2_filename"]
+
+This returns the metadata for each slide.
--- SlideServer.py
+++ SlideServer.py
@@ -1,13 +1,27 @@
 import openslide
 import flask
+import os
+import json
+import hashlib
 from werkzeug.utils import secure_filename
 
 app = flask.Flask(__name__)
 
-# where to put slides
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# where to put and get slides
+app.config['UPLOAD_FOLDER'] = "/data/images/"
 
 ALLOWED_EXTENSIONS = set(['svs','tif'])
+
+def file_md5(fileName):
+    m = hashlib.md5()
+    blocksize = 2**20
+    with open(fileName, "rb") as f:
+        while True:
+            buf = f.read(blocksize)
+            if not buf:
+                break
+            m.update(buf)
+    return m.hexdigest()
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -15,9 +29,13 @@ def allowed_file(filename):
 
 # given a path, get metadata
 def getMetadata(filename):
+    # TODO consider restricting filepath
     metadata = {}
-    metadata['filename'] = filename
-    slide = openslide.OpenSlide(filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    if not os.path.isfile(filepath):
+        return {"error": "No such file"}
+    metadata['filename'] = filepath
+    slide = openslide.OpenSlide(filepath)
     slideData = slide.properties
     metadata['mpp-x'] = slideData.get(openslide.PROPERTY_NAME_MPP_X, None)
     metadata['mpp-y'] = slideData.get(openslide.PROPERTY_NAME_MPP_Y, None)
@@ -26,7 +44,7 @@ def getMetadata(filename):
     metadata['vendor'] = slideData.get(openslide.PROPERTY_NAME_VENDOR, None)
     metadata['level_count'] = int(slideData.get('level_count', 1))
     metadata['objective'] = float(slideData.get("aperio.AppMag", None))
-    metadata['md5sum'] = file_md5(filename)
+    metadata['md5sum'] = file_md5(filepath)
     return metadata
 
 # given a list of path, get metadata for each
@@ -38,7 +56,7 @@ def getMetadataList(filenames):
 
 ## routes
 
-# upload, graciously stolen from pocoo
+# upload, file as file:
 @app.route('/upload', methods=['POST'])
 def upload_file():
     # check if the post request has the file part
@@ -54,13 +72,17 @@ def upload_file():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath, filename))
+        file.save(filepath, filename)
         return filepath
 
-@app.route("/data/one")
-def uploadSlide():
-    pass
+@app.route("/test", methods=['GET'])
+def testRoute():
+    return '{"Status":"up"}'
 
-@app.route("/data/many")
-def uploadSlide():
-    pass
+@app.route("/data/one/<filepath>", methods=['GET'])
+def singleSlide(filepath):
+    return json.dumps(getMetadata(filepath))
+
+@app.route("/data/many/<filepathlist>", methods=['GET'])
+def multiSlide(filepathlist):
+    return json.dumps(getMetadataList(json.loads(filepathlist)))

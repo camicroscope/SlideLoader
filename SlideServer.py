e4e00e1fecdc1bf6abffb2862b6b2e6f1311fad0
Ryan Birmingham <birm@rbirm.us>
2018-10-10 15:42:57 -0400

    debug delayed dile

--- SlideServer.py
+++ SlideServer.py
@@ -4,6 +4,7 @@ import os
 import json
 import hashlib
 import flask_cors
+import sys
 from werkzeug.utils import secure_filename
 
 app = flask.Flask(__name__)
@@ -73,10 +74,11 @@ def upload_file():
         </form>
         '''
     # check if the post request has the file part
+    print(flask.request.files, file=sys.stderr)
     if 'file' not in flask.request.files:
         return flask.Response(json.dumps({"error": "NOT UPLOADED: No File"}), status=400)
     file = flask.request.files['file']
-    filename = flask.request.form["filename"] or file.filename
+    filename = flask.request.form.get("filename", file.filename)
     if filename == '':
         return flask.Response(json.dumps({"error": "NOT UPLOADED: No Filename Given"}), status=400)
     if file and allowed_file(filename):

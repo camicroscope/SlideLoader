72461e2f3a652dcba7904eb458033a393eca9c3b
Ryan Birmingham <birm@rbirm.us>
2018-10-17 17:24:31 -0400

    start of file upload in chunk method

--- SlideServer.py
+++ SlideServer.py
@@ -5,6 +5,8 @@ import json
 import hashlib
 import flask_cors
 import sys
+import random
+import base64
 from werkzeug.utils import secure_filename
 
 app = flask.Flask(__name__)
@@ -12,9 +14,12 @@ flask_cors.CORS(app)
 
 # where to put and get slides
 app.config['UPLOAD_FOLDER'] = "/data/images/"
+app.config['TOKEN_SIZE'] = 10
 app.config['SECRET_KEY'] = os.urandom(24)
 
 ALLOWED_EXTENSIONS = set(['svs','tif'])
+
+__IN_PROGRESS = {}
 
 def file_md5(fileName):
     m = hashlib.md5()
@@ -90,6 +95,74 @@ def upload_file():
             return flask.Response(json.dumps({"error": "NOT UPLOADED: File Exists"}), status=400)
     else:
         return flask.Response(json.dumps({"error": "NOT UPLOADED: Server Error"}), status=500)
+
+## start a file upload by registering the intent to upload, get a token to be used in future upload requests
+@app.route('/start/upload', methods=['POST', "GET"])
+def upload_file():
+    body = flask.request.get_json()
+    key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(app.config['TOKEN_SIZE']))
+    # regenrate if we happen to collide
+    while key in __IN_PROGRESS:
+        key = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(app.config['TOKEN_SIZE']))
+    # check filename
+    filename = body['filename']
+    if filename and allowed_file(filename):
+        filename = secure_filename(filename)
+        if not os.path.isfile(filepath):
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            # register token info
+            __IN_PROGRESS[key] = {'filepath': filepath, 'last_seen_offset':none}
+            # create empty file
+            open(filepath, 'a').close()
+            return flask.Response(json.dumps({"upload_token": key}), status=200)
+        else:
+            # BAD FILEPATH
+            return flask.Response(json.dumps({"error": "Bad or unallowed filepath: " + filepath}), status=400)
+    else:
+        # MISSING FILE INFO
+        return flask.Response(json.dumps({"error": "Filename missing or not allowed"}), status=400)
+
+## using the token from the start upload endpoint, post data given offset.
+@app.route('/upload/continue/<token>', methods=['POST', "GET"])
+def continue_file(token):
+    if flask.request.method == "GET":
+        if token in __IN_PROGRESS:
+            return flask.Response(json.dumps(__IN_PROGRESS[token]))
+        else:
+            return flask.Response(json.dumps({"error": "Token not recognized"}), status=400)
+    else:
+        if token in __IN_PROGRESS:
+            body = flask.request.get_json()
+            offset = body.offset or 0
+            if not data in body:
+                return flask.Response(json.dumps({"error": "File data not found in body"}), status=400)
+            data = base64.b64decode(body.data)
+            process = __IN_PROGRESS[token]
+            if not os.path.isfile(filepath):
+                return flask.Response(json.dumps({"error": "File is missing from server"}), status=500)
+            f = open(process['filepath'], "wb")
+            f.seek(offset)
+            f.write(data)
+            f.close()
+            __IN_PROGRESS['last_seen_offset'] = offset
+        else:
+            return flask.Response(json.dumps({"error": "Token not recognized"}), status=400)
+
+## end the upload, by removing the in progress indication; locks further modification
+@app.route('/upload/finish/<token>', methods=['POST', "GET"])
+def finish_upload(token):
+    body = flask.request.get_json()
+    if token in __IN_PROGRESS:
+        s = __IN_PROGRESS[token]
+        del __IN_PROGRESS[token]
+        return flask.Response(json.dumps({"ended": s}))
+    else:
+        return flask.Response(json.dumps({"error": "Token not recognized"}), status=400)
+
+
+    # check for token
+    # get info associated with token
+    # move the file out of temp to upload dir
 
 
 @app.route("/test", methods=['GET'])

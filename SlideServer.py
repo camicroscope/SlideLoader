7978c7033d96d0f404a430703477b54eefc373bb
Ryan Birmingham <birm@rbirm.us>
2018-10-18 16:31:20 -0400

    try flusing on write

--- SlideServer.py
+++ SlideServer.py
@@ -107,6 +107,7 @@ def continue_file(token):
             f = open(tmppath, "wb")
             f.seek(int(offset))
             f.write(data)
+            f.flush()
             f.close()
             return flask.Response(json.dumps({"status": "OK"}), status=200)
     else:

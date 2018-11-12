e6fdb0580d3692d589cff7714d746801c5d29792
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-11-12 16:22:07 -0500

    parameter

--- upload.sh
+++ upload.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-# TODO: request local dir param
+if [[ $# -eq 0 ]]
+  then
+    echo "Please provide image source directory file path"
+    exit 0
+fi
+
+if [[ ! -d "$1" ]]; then
+  echo "$1 does not exist; are you sure this is the full file path?"
+  exit 1
+fi
 
 # Upload Slides
-docker cp ~/slides/. ca-load:/data/images/
+docker cp $1/. ca-load:/data/images/
 
 # Load to Database
-for file in ~/slides/*.svs; do
+for file in $1/*.svs; do
     slide="$(basename "$file")"
     echo $slide
     docker exec -it ca-load python3 /var/www/upload.py "$slide" "/data/images/"

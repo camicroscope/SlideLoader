cee65e2ed76e5dcbb29a457160b4da2e56651e4b
Ryan Birmingham <birm@rbirm.us>
2020-05-26 22:34:31 -0400

    start new Omniloader

---
+++ OmniLoad.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import openslide # to get required slide metadata
+import csv # to read csv
+import argparse # to read arguments
+import time # for timestamp
+import os # for os/fs systems
+import json # for json in and out
+
+parser = argparse.ArgumentParser(description='Load slides or results to caMicroscope.')
+# read in collection
+parser.add_argument('-i', type=str, default="slide", choices=['slide', 'heatmap', 'mark', 'user'],
+                    help='Input type (collection)')
+# read in filepath
+parser.add_argument('-f', type=str, default="manifest.csv",
+                    help='Input file')
+# read in dest type
+parser.add_argument('-o', type=str, default="mongo", choices=['mongo', 'file', 'api', 'pathdb'],
+                    help='Output destination type')
+# read in dest uri or equivalent
+parser.add_argument('-d', type=str, default="ca-mongo",
+                    help='Output destination')
+
+args = parser.parse_args()
+print(args)
+
+def openslidedata(manifest):
+    for img in manifest:
+        img['location'] = img['location'] or img['filename'] or img['file']
+        slide = openslide.OpenSlide(img['location'])
+        slideData = slide.properties
+        img['mpp-x'] = slideData.get(openslide.PROPERTY_NAME_MPP_X, None)
+        img['mpp-x'] = slideData.get(openslide.PROPERTY_NAME_MPP_Y, None)
+        img['mpp'] = img['mpp-x'] or img['mpp-x'] or None
+        img['height'] = slideData.get(openslide.PROPERTY_NAME_BOUNDS_HEIGHT, None)
+        img['width'] = slideData.get(openslide.PROPERTY_NAME_BOUNDS_WIDTH, None)
+        img['vendor'] = slideData.get(openslide.PROPERTY_NAME_VENDOR, None)
+        img['level_count'] = int(slideData.get('level_count', 1))
+        img['objective'] = float(slideData.get("aperio.AppMag", None))
+        img['timestamp'] = time.time()
+        # required values which are often unused
+        img['study'] = img.get('study', "")
+        img['specimen'] = img.get('specimen', "")
+    return manifest
+
+manifest = []
+
+# context for file
+with open(args.f, 'r') as f:
+    # determine type
+    ext = os.path.splitext(args.f)[1]
+    if (ext==".csv"):
+        reader = csv.DictReader(f)
+        manifest = [row for row in reader]
+    elif (ext==".json"):
+        manifest = json.load(f)
+    else:
+        raise NotImplementedError("Extension: " + ext + " Unsupported")
+
+# if it's slide, do the slide info fill in
+if (args.i == "slide"):
+    manifest = openslidedata(manifest)
+
+# perform validation (!!)
+
+# if dest is mongo, connect
+
+# if dest is an api, try
+# ^^ if 401, ask for a token
+
+# if dest is pathdb, ask for token
+
+# if dest is file, then write them

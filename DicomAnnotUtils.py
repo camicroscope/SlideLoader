109a7ab49be20d8f48869d0672aaf0b46be2180c
Birm <birm@rbirm.us>
2024-04-05 21:47:32 -0400

    untested rect, ellipse, point

--- DicomAnnotUtils.py
+++ DicomAnnotUtils.py
@@ -37,7 +37,10 @@ annotTemplate = {
         "image": {
             "slide": "ADD_CAMIC_SLIDE_ID",
             "dicom-ReferencedSOPClassUID": "",
-            "dicom-ReferencedSOPInstanceUID": ""
+            "dicom-ReferencedSOPInstanceUID": "",
+            "dicom-study": "",
+            "dicom-series": "",
+            "dicom-instance": "",
         },
         "analysis": {
             "source": "human", # may not be true!
@@ -223,6 +226,13 @@ def _polygon_perimeter(vertices):
         perimeter += np.linalg.norm(np.array(vertices[i]) - np.array(vertices[j]))
     return perimeter
 
+def convert_ellipse(x1_major,y1_major,x2_major,y2_major,x1_minor,y1_minor,x2_minor,y2_minor):    
+    center_x = (x1_major + x2_major) / 2
+    center_y = (y1_major + y2_major) / 2
+    major_axis_length = np.sqrt((x2_major - x1_major)**2 + (y2_major - y1_major)**2) / 2
+    minor_axis_length = np.sqrt((x2_minor - x1_minor)**2 + (y2_minor - y1_minor)**2) / 2
+    rotation = np.arctan2(y2_major - y1_major, x2_major - x1_major)
+    return center_x, center_y, major_axis_length, minor_axis_length, rotation
 
 def dicomToCamic(annot_path, image_path, output_file, slide_id=None, file_mode=False):
     slide_ds = pydicom.dcmread(image_path)
@@ -240,6 +250,7 @@ def dicomToCamic(annot_path, image_path, output_file, slide_id=None, file_mode=F
     # TODO this is just for POLYGON. generalize later.
     res = []
     for x in annot_ds.AnnotationGroupSequence:
+
         exported_annot = deepcopy(annotTemplate)
         exported_annot['properties']['annotations']['name'] = annot_ds.ContentLabel
         exported_annot['provenance']['analysis']['name'] = annot_ds.ContentLabel
@@ -249,55 +260,127 @@ def dicomToCamic(annot_path, image_path, output_file, slide_id=None, file_mode=F
             exported_annot['provenance']['image']['slide'] = slide_id
         exported_annot['provenance']['image']['dicom-ReferencedSOPClassUID'] = annot_ds.ReferencedImageSequence[0].ReferencedSOPClassUID
         exported_annot['provenance']['image']['dicom-ReferencedSOPInstanceUID'] = annot_ds.ReferencedImageSequence[0].ReferencedSOPInstanceUID
-        n = x.PointCoordinatesData
-        m = np.frombuffer(n, dtype=np.float32)
-        coordinates_array = (np.array(m).reshape(-1, 2)) / [slide_width,slide_height] # normalize coordinates for camicroscope
-        # split into different geometry objects Index List
-        indexList = np.frombuffer(x.LongPrimitivePointIndexList, dtype=np.int32)
-        #print("IndexList", indexList)
-        #print("len Index List", len(indexList))
-        #print("coordinates_array shape", coordinates_array.shape)
-        if len(indexList) > 1:
-            # split
-            prevIndex = 0
-            for idx in indexList[1:]:
-                end_idx = int((idx-1)/2)
-                #print("prev", prevIndex, "idx", idx)
-                # make a thing 
-                points = coordinates_array[prevIndex:end_idx, :]
-                points = np.concatenate((points, [points[0]]))
-                #print('len(points)', len(points))
+        exported_annot['provenance']['image']["dicom-study"] = annot_ds.StudyInstanceUID
+        exported_annot['provenance']['image']["dicom-series"] = annot_ds.SeriesInstanceUID
+        exported_annot['provenance']['image']["dicom-instance"] = annot_ds.SOPInstanceUID
+        
+        # handle other cases first
+        if x.GraphicType == "ELLIPSE":
+            n = x.PointCoordinatesData
+            m = np.frombuffer(n, dtype=np.float32)
+            # sets of 4 points, 8 numbers
+            for i in range(8, len(m), 8):
+                ellipse_points = n[i-8, i]
+                x1_major = ellipse_points[0]/slide_width
+                y1_major = ellipse_points[1]/slide_height
+                x2_major = ellipse_points[2]/slide_width
+                y2_major = ellipse_points[3]/slide_height
+                x1_minor = ellipse_points[4]/slide_width
+                y1_minor = ellipse_points[5]/slide_height
+                x2_minor = ellipse_points[6]/slide_width
+                y2_minor = ellipse_points[7]/slide_height
+                center_x, center_y, major_axis_length, minor_axis_length, rotation = convert_ellipse(x1_major,y1_major,x2_major,y2_major,x1_minor,y1_minor,x2_minor,y2_minor)
+                newFeature = deepcopy(featureTemplate)
+                newFeature['geometry']['type'] = "Ellipse"
+                newFeature['geometry']['coordinates'] = [center_x, center_y]
+                newFeature['geometry']["radius"] = [major_axis_length,minor_axis_length],
+                newFeature['geometry']["rotation"] = rotation
+                newFeature['bound']['type'] = "Point"
+                newFeature['bound']['coordinates'] = [center_x, center_y]
+                exported_annot['geometries']['features'] = [newFeature]
+                exported_annot['provenance']['analysis']['execution_id'] = "_DICOM_" + _generate_random_string(10)
+                res.append(deepcopy(exported_annot))       
+        elif x.GraphicType == "POINT":
+            # sets of 1 point, 2 numbers
+            n = x.PointCoordinatesData
+            m = np.frombuffer(n, dtype=np.float32)
+            # sets of 4 points, 8 numbers
+            for i in range(2, len(m), 2):
+                point = n[i-2, i]
+                center_x = point[0]/slide_width
+                center_y = point[1]/slide_height
+                newFeature = deepcopy(featureTemplate)
+                newFeature['geometry']['type'] = "Point"
+                newFeature['geometry']['coordinates'] = [center_x, center_y]
+                newFeature['bound']['type'] = "Point"
+                newFeature['bound']['coordinates'] = [center_x, center_y]
+                exported_annot['geometries']['features'] = [newFeature]
+                exported_annot['provenance']['analysis']['execution_id'] = "_DICOM_" + _generate_random_string(10)
+                res.append(deepcopy(exported_annot))  
+        elif x.GraphicType == "RECTANGLE":
+            n = x.PointCoordinatesData
+            m = np.frombuffer(n, dtype=np.float32)
+            # sets of 4 points, 8 numbers
+            for i in range(8, len(m), 8):
+                rect_points = n[i-8, i]
+                x1 = rect_points[0]/slide_width
+                y1 = rect_points[1]/slide_height
+                x2 = rect_points[2]/slide_width
+                y2 = rect_points[3]/slide_height
+                x3 = rect_points[4]/slide_width
+                y3 = rect_points[5]/slide_height
+                x4 = rect_points[6]/slide_width
+                y4 = rect_points[7]/slide_height
+                newFeature = deepcopy(featureTemplate)
+                newFeature['geometry']['type'] = "Polygon"
+                newFeature['geometry']['coordinates'] = [[x1,y1], [x2,y2], [x3,y3], [x4,y4], [x1,y1]]
+                newFeature['geometry']["radius"] = [major_axis_length,minor_axis_length],
+                newFeature['geometry']["rotation"] = rotation
+                newFeature['bound']['type'] = "Polygon"
+                newFeature['bound']['coordinates'] = [[x1,y1], [x2,y2], [x3,y3], [x4,y4], [x1,y1]]
+                exported_annot['geometries']['features'] = [newFeature]
+                exported_annot['provenance']['analysis']['execution_id'] = "_DICOM_" + _generate_random_string(10)
+                res.append(deepcopy(exported_annot))
+        elif x.GraphicType == "POLYGON" or x.GraphicType == "POLYLINE":
+            n = x.PointCoordinatesData
+            m = np.frombuffer(n, dtype=np.float32)
+            coordinates_array = (np.array(m).reshape(-1, 2)) / [slide_width,slide_height] # normalize coordinates for camicroscope
+            # split into different geometry objects Index List
+            indexList = np.frombuffer(x.LongPrimitivePointIndexList, dtype=np.int32)
+            #print("IndexList", indexList)
+            #print("len Index List", len(indexList))
+            #print("coordinates_array shape", coordinates_array.shape)
+            if len(indexList) > 1:
+                # split
+                prevIndex = 0
+                for idx in indexList[1:]:
+                    end_idx = int((idx-1)/2)
+                    #print("prev", prevIndex, "idx", idx)
+                    # make a thing 
+                    points = coordinates_array[prevIndex:end_idx, :]
+                    points = np.concatenate((points, [points[0]]))
+                    #print('len(points)', len(points))
+                    if len(points) > 0:
+                        newFeature = deepcopy(featureTemplate)
+                        newFeature['geometry']['coordinates'].append(points.tolist())
+                        newFeature['bound']['coordinates'].append(_makeBound(points))
+                        exported_annot['geometries']['features'] = [newFeature]
+                        exported_annot['provenance']['analysis']['execution_id'] = "_DICOM_" + _generate_random_string(10)
+                        res.append(deepcopy(exported_annot))
+                    prevIndex = end_idx
+                    # and the bound
+                # then add the last one
+                points = coordinates_array[prevIndex:, :]
                 if len(points) > 0:
                     newFeature = deepcopy(featureTemplate)
                     newFeature['geometry']['coordinates'].append(points.tolist())
                     newFeature['bound']['coordinates'].append(_makeBound(points))
-                    exported_annot['geometries']['features'] = [newFeature]
                     exported_annot['provenance']['analysis']['execution_id'] = "_DICOM_" + _generate_random_string(10)
+                    exported_annot['geometries']['features'] = [newFeature]
                     res.append(deepcopy(exported_annot))
-                prevIndex = end_idx
-                # and the bound
-            # then add the last one
-            points = coordinates_array[prevIndex:, :]
-            if len(points) > 0:
+            else:
+                # whole thing at once. Only do area and circumference here.
+                points = coordinates_array
                 newFeature = deepcopy(featureTemplate)
                 newFeature['geometry']['coordinates'].append(points.tolist())
                 newFeature['bound']['coordinates'].append(_makeBound(points))
-                exported_annot['provenance']['analysis']['execution_id'] = "_DICOM_" + _generate_random_string(10)
                 exported_annot['geometries']['features'] = [newFeature]
+                area = _polygon_perimeter(points.tolist()) * mpp_x * slide_width
+                perimeter = _polygon_area(points.tolist()) * (mpp_x * slide_width)**2
+                exported_annot['properties']['annotations']['circumference'] = str(area)+ " μm"
+                exported_annot['properties']['annotations']['area'] = str(perimeter) + " μm²"
+                exported_annot['provenance']['analysis']['execution_id'] = "_DICOM_" + _generate_random_string(10)
                 res.append(deepcopy(exported_annot))
-        else:
-            # whole thing at once. Only do area and circumference here.
-            points = coordinates_array
-            newFeature = deepcopy(featureTemplate)
-            newFeature['geometry']['coordinates'].append(points.tolist())
-            newFeature['bound']['coordinates'].append(_makeBound(points))
-            exported_annot['geometries']['features'] = [newFeature]
-            area = _polygon_perimeter(points.tolist()) * mpp_x * slide_width
-            perimeter = _polygon_area(points.tolist()) * (mpp_x * slide_width)**2
-            exported_annot['properties']['annotations']['circumference'] = str(area)+ " μm"
-            exported_annot['properties']['annotations']['area'] = str(perimeter) + " μm²"
-            exported_annot['provenance']['analysis']['execution_id'] = "_DICOM_" + _generate_random_string(10)
-            res.append(deepcopy(exported_annot))
     
     if file_mode:
         n = 0
